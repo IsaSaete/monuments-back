3ca5132c1782ef48f2dc967064c35b4531499469
Isabel Sáenz <isasaete@gmail.comn>
2025-04-03 17:23:29 +0200

    Add middleware handleEndPointNotFound & its test

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import handleEndpointNotFound from "./middlewares/handleEndpointNotFound";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
+
+app.use(handleEndpointNotFound);
 
 export default app;
