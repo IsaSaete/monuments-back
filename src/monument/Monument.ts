eebf1ec8119a22cbd1f2036dd60c1d72983ecd95
Isabel Sáenz <isasaete@gmail.comn>
2025-04-03 12:50:40 +0200

    Modify Monument test

--- src/monument/Monument.ts
+++ src/monument/Monument.ts
@@ -2,9 +2,9 @@ import { v4 as uuidv4 } from "uuid";
 import { Location, MonumentStructure } from "./types";
 
 class Monument implements MonumentStructure {
-  id: string;
-  country: string;
-  city: string;
+  public id: string;
+  public country: string;
+  public city: string;
 
   constructor(
     public name: string,
