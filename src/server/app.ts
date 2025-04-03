708eb6ecd03410d4da53acd340de41b38e8390a2
Isabel Sáenz <isasaete@gmail.comn>
2025-04-03 20:31:01 +0200

    Add monumentRouter

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,12 +2,15 @@ import express from "express";
 import handleEndpointNotFound from "./middlewares/handleEndpointNotFound";
 import morgan from "morgan";
 import checkHealthStatus from "./middlewares/checkHealthStatus";
+import monumentRouter from "../monument/router/monumentRouter";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.get("/", checkHealthStatus);
+
+app.use("/monuments", monumentRouter);
 
 app.use(handleEndpointNotFound);
 
