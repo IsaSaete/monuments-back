ecff7765efbaa3e6108e625d4d514682bc5f215a
Isabel Sáenz <isasaete@gmail.comn>
2025-04-05 13:03:44 +0200

    Add middleware cors

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,10 +3,13 @@ import handleEndpointNotFound from "./middlewares/handleEndpointNotFound";
 import morgan from "morgan";
 import checkHealthStatus from "./middlewares/checkHealthStatus";
 import monumentRouter from "../monument/router/monumentRouter";
+import cors from "cors";
 
 const app = express();
 
 app.use(morgan("dev"));
+
+app.use(cors({ origin: "https://https://monumentisa.netlify.app/" }));
 
 app.get("/", checkHealthStatus);
 
